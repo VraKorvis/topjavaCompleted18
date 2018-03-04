99eea195fbcf1db84e7a8ec513b8c8603005d778
VraKorvis <it.korvis@gmail.com>
2018-03-05 03:00:05 +0400

    move hardcode and lastid from MealsDataBase, add CRUD methods in MealsDao

--- src/main/java/ru/javawebinar/topjava/dao/MealsDao.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealsDao.java
@@ -2,9 +2,13 @@ package ru.javawebinar.topjava.dao;
 
 import ru.javawebinar.topjava.model.Meal;
 
+import java.util.Collection;
+
 public interface MealsDao {
 
+    Meal getId(int id);
     void addOrUpdate(Meal meal);
     void delete(int id);
+    Collection<Meal> getAll();
 
 }
--- src/main/java/ru/javawebinar/topjava/dao/MealsDaoImpl.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealsDaoImpl.java
@@ -3,7 +3,12 @@ package ru.javawebinar.topjava.dao;
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collection;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -11,21 +16,36 @@ public class MealsDaoImpl implements MealsDao {
 
     private static final Logger log = getLogger(MealsDaoImpl.class);
 
-    private ConcurrentHashMap<Integer, Meal> meals;
+    private static final AtomicInteger lastId = new AtomicInteger();
 
-    public MealsDaoImpl() {
-        this.meals = MealsDataBase.getMeals();
+    private static final Map<Integer, Meal> meals;
+
+    static {
+        meals = new ConcurrentHashMap<>();
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 28, 9, 0), "Завтрак", 500));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 28, 12, 0), "Обед", 1000));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 28, 20, 0), "Ужин", 501));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 29, 11, 0), "Завтрак", 1000));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 29, 14, 0), "Обед", 500));
+        meals.put(lastId.get(), new Meal(lastId.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 29, 20, 0), "Ужин", 500));
     }
 
-    public ConcurrentHashMap<Integer, Meal> getMeals() {
-        return meals;
+    @Override
+    public Meal getId(int id) {
+        return null;
     }
 
     @Override
     public void addOrUpdate(Meal meal) {
         log.debug("add/update meal " + meal);
         if (meal.getId() == 0) {
-            meal.setId(MealsDataBase.getLastId().getAndIncrement());
+            meal.setId(lastId.incrementAndGet());
         }
         meals.put(meal.getId(), meal);
     }
@@ -34,6 +54,11 @@ public class MealsDaoImpl implements MealsDao {
     public void delete(int id) {
         log.debug("removing meal by id " + id);
         meals.remove(id);
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return meals.values();
     }
 
 }
