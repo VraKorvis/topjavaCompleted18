fdcd13ed576d9b8059ff559cc4230d96ea5c3052
it.korvis@gmail.com <qwas1987>
2018-05-13 20:17:29 +0400

    meal add annot @JoinColumn

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -39,6 +39,7 @@ public class Meal extends AbstractBaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @NotNull
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Meal() {
